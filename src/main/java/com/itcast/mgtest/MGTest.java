f1c80b2fdf24d8c8da884ab0bec671cc878c1269
baijun <baijun@itcast.cn>
2019-03-01 16:20:16 +0800

    第一次提交

---
+++ src/main/java/com/itcast/mgtest/MGTest.java
@@ -0,0 +1,11 @@
+package com.itcast.mgtest;
+
+import java.io.Serializable;
+
+public class MGTest implements Serializable {
+
+	public static void main(String[] args) {
+		System.out.println("aaaa");
+	}
+
+}
