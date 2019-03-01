8798f44b94c7427c8b4b2ee08142d7cfb64611cc
baijun <baijun@itcast.cn>
2019-03-01 16:48:36 +0800

    第三次

---
+++ src/test/java/com/itcast/mgtest/MGTestTest.java
@@ -0,0 +1,14 @@
+package com.itcast.mgtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MGTestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
